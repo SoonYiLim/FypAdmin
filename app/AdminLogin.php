b9b38c23e234061f9c2d47a8fa36ad4e749cf0d3
SoonYiLim <56568354+SoonYiLim@users.noreply.github.com>
2020-10-22 13:45:02 +0800

    Initial commit

---
+++ app/AdminLogin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\AdminResetPasswordNotification;
+use Str;
+
+class AdminLogin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'admins';
+    protected $guard = 'admin';
+
+    // One to many relationship
+    public function meals()
+    {
+        return $this->hasMany('App\Meal');
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new AdminResetPasswordNotification($token));
+    }
+
+    public function generateToken()
+    {
+        $this->api_token = Str::random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
+}
